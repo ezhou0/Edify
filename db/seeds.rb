2f874a1420016a17cd9d89e183cd2e7d95a279f5
Edwin <edwinzhou99@gmail.com>
2021-03-22 12:00:49 -0700

    add artist and album seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,20 @@
 userBase = User.create(username: 'username', email: 'email.com', password: 'password', gender: 'Male' )
 user1 = User.create(username: 'username1', email: 'email1.com', password: 'password1', gender: 'Female' )
 user2 = User.create(username: 'username2', email: 'email2.com', password: 'password2', gender: 'Non-binary')
+
+
+#artists
+artist1 = Artist.create!(name: 'artist1', description: 'this is an artist')
+artist2 = Artist.create!(name: 'artist2')
+
+#TODOlink artist picture and attach photo
+
+#albums
+
+album1 = Album.create!(name: 'artist1 album' year: 2021, artist_id: artist1.id, description: '1st album for artist1')
+album2 = Album.create!(name: 'artist1 2nd album' year: 2021, artist_id: artist1.id, description: '2nd album for artist1')
+album3 = Album.create!(name: 'artist2 album' year: 2021, artist_id: artist2.id, description: '1st album for artist2')
+
+#TODO add album covers
+
+
