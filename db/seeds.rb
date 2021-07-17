655c4154f1fb93bc524831a5b5db89c107451ea9
Edwin <edwinzhou99@gmail.com>
2021-07-17 11:39:46 -0700

    update seeds for viewing

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,8 +15,8 @@ user2 = User.create(username: 'username2', email: 'email2.com', password: 'passw
 
 #artists
 # artistBase = Artist.create!(name:'artistBase')
-artist1 = Artist.create!(name: 'artist1', description: 'this is an artist')
-artist2 = Artist.create!(name: 'artist2')
+artist1 = Artist.create!(name: 'Hiss', description: 'this is an artist')
+artist2 = Artist.create!(name: 'Elvis Purrsley')
 
 # artistBasepic = open("https://edify-app-seeds.s3-us-west-1.amazonaws.com/Edifypics/artistbase.jpeg")
 artist1pic = open("https://edify-app-seeds.s3-us-west-1.amazonaws.com/Edifypics/catartist.jpeg")
@@ -29,9 +29,9 @@ artist2.artist_photo.attach(io: artist2pic, filename:'guitarcat.jpeg' )
 
 #albums
 
-album1 = Album.create!(name: 'artist1 album', year: 2021, artist_id: artist1.id, description: '1st album for artist1')
-album2 = Album.create!(name: 'artist1 2nd album', year: 2021, artist_id: artist1.id, description: '2nd album for artist1')
-album3 = Album.create!(name: 'artist2 album', year: 2021, artist_id: artist2.id, description: '1st album for artist2')
+album1 = Album.create!(name: 'Lick it up', year: 2021, artist_id: artist1.id, description: '1st album for artist1')
+album2 = Album.create!(name: 'Kiss', year: 2021, artist_id: artist1.id, description: '2nd album for artist1')
+album3 = Album.create!(name: 'Moody Blue', year: 2021, artist_id: artist2.id, description: '1st album for artist2')
 
 #link album covers
 
@@ -45,12 +45,12 @@ album2.album_photo.attach(io: album2pic, filename: 'catalbum2.png')
 album3.album_photo.attach(io: album3pic, filename: 'catalbum3.jpeg')
 
 #songs
-song1 = Song.create!(title: 'song1', album_id: album1.id, track_number: 1)
-song2 = Song.create(title: 'song2', album_id: album1.id, track_number: 2)
-song3 = Song.create(title: 'song3', album_id: album2.id, track_number: 1)
-song4 = Song.create(title: 'song4', album_id: album2.id, track_number: 2)
-song5 = Song.create(title: 'song5', album_id: album3.id, track_number: 1)
-song6 = Song.create(title: 'song6', album_id: album3.id, track_number: 2)
+song1 = Song.create!(title: 'Dreams', album_id: album1.id, track_number: 1)
+song2 = Song.create(title: 'Good Morning', album_id: album1.id, track_number: 2)
+song3 = Song.create(title: 'Kawaii', album_id: album2.id, track_number: 1)
+song4 = Song.create(title: 'Nimbus', album_id: album2.id, track_number: 2)
+song5 = Song.create(title: 'Places', album_id: album3.id, track_number: 1)
+song6 = Song.create(title: 'Summer', album_id: album3.id, track_number: 2)
 
 #songs mp3
 song1mp3 = open('https://edify-app-seeds.s3.us-west-1.amazonaws.com/edifySongs/dreams.mp3')
@@ -71,6 +71,7 @@ song6.audio.attach(io: song6mp3, filename: 'summer.mp3')
 #playlists
 playlist1 = Playlist.create!(name: 'first playlist', author_id: userBase.id)
 playlist2 = Playlist.create!(name: 'second playlist', author_id: userBase.id)
+playlist3 = Playlist.create!(name: 'a chill time', author_id: userBase.id)
 
 #playlist songs
 
@@ -80,6 +81,11 @@ ps3 = PlaylistSong.create!(song_id: song3.id, playlist_id: playlist1.id)
 ps4 = PlaylistSong.create!(song_id: song4.id, playlist_id: playlist2.id)
 ps5 = PlaylistSong.create!(song_id: song5.id, playlist_id: playlist2.id)
 ps6 = PlaylistSong.create!(song_id: song6.id, playlist_id: playlist2.id)
+ps7 = PlaylistSong.create!(song_id: song6.id, playlist_id: playlist3.id)
+ps8 = PlaylistSong.create!(song_id: song2.id, playlist_id: playlist3.id)
+ps9 = PlaylistSong.create!(song_id: song3.id, playlist_id: playlist3.id)
+ps10 = PlaylistSong.create!(song_id: song4.id, playlist_id: playlist3.id)
+
 
 
 
